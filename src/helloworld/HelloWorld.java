6c9e1a044f1cc4bd4d31fefd810ca4e53fcb2488
rskob <rskob@MSI>
2017-09-27 11:47:26 -0600

    Initialized project

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -16,6 +16,12 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        hiFriends("Bilbo");
     }
+    
+    public static void hiFriends(String a){
+        System.out.println("Welcome to the shire " + a);
+    }
+    
     
 }
